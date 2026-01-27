cbd99053744aed95e7287241a6bfe8b62a37ebc0
Vinícius Silva Fonseca <vinicius.s.fonseca@outlook.com>
2026-01-27 18:02:12 -0300

    Adição dos produtos em /utils/productDataBase

---
+++ src/utils/productsDataBase.js
@@ -0,0 +1,52 @@
+const productsDataBase = [
+  {
+    id: 1,
+    name: "AirPods Apple Fonses de ouvido",
+    price: 1499.0,
+    img: "",
+  },
+  {
+    id: 2,
+    name: "Capa de silicone para Iphone 8/7 cor Areia - rosa",
+    price: 299.0,
+    img: "",
+  },
+  {
+    id: 3,
+    name: "Apple Pencil",
+    price: 729.0,
+    img: "",
+  },
+  {
+    id: 4,
+    name: "Magic Mouse 2 - Prateado",
+    price: 549.0,
+    img: "",
+  },
+  {
+    id: 5,
+    name: "Caixa prateada de alumínio com pulseira esportiva branca",
+    price: 2899.0,
+    img: "",
+  },
+  {
+    id: 6,
+    name: "Cabo de lightning para USB (1m)",
+    price: 149.0,
+    img: "",
+  },
+  {
+    id: 7,
+    name: "Smart Keyboard para iPad Pro 12,9 polegadas - Inglês (EUA)",
+    price: 1099.0,
+    img: "",
+  },
+  {
+    id: 8,
+    name: "Carregador USB de 5W Apple",
+    price: 149.0,
+    img: "",
+  },
+];
+
+export default productsDataBase;
